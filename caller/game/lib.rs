b8b206eef7962d492366fae4290b4c0548c98e2a
Brian Anderson <andersrb@gmail.com>
2021-01-16 22:14:16 +0000

    .

--- caller/game/lib.rs
+++ caller/game/lib.rs
@@ -22,28 +22,24 @@ mod game {
     }
 
     #[ink(storage)]
-    pub struct Game {
-        value: AccountId,
-    }
+    pub struct Game { }
 
     impl Game {
         #[ink(constructor)]
         pub fn default() -> Self {
-            let program_id = "4cfac7f74c6233449b5e54ba070231dd94c71b89505482cd910000656258d3ed";
-            let program_id = hex::decode(program_id).unwrap();
-            let program_id = AccountId::try_from(&program_id[..]).unwrap();
-            //            ink_env::debug_println(&format!("AccountId {:?}", program_id));
-
-            Game {           
-                value: program_id,
-            }
+            Game { }
         }
 
         #[ink(message, payable)]
         pub fn run_game_test(&mut self, program_id: AccountId) -> Result<bool, Error> {
-            let caller = self.env().caller();
-            ink_env::debug_println(&format!("caller: {:?}", caller));
-            ink_env::debug_println(&format!("calling flip on {:?}", program_id));
+            let env = self.env();
+            /*ink_env::debug_println(&format!("caller: {:?}", env.caller()));
+            ink_env::debug_println(&format!("transferred balance: {:?}", env.transferred_balance()));
+            let gas_left = env.gas_left();
+            ink_env::debug_println(&format!(*/
+            ink_env::debug_println(&format!("env: {:?}", env));
+
+            //ink_env::debug_println(&format!("calling flip on {:?}", program_id));
 /*
             let return_value: () = build_call::<DefaultEnvironment>()
                 .callee(program_id) 
@@ -59,7 +55,7 @@ mod game {
             */
             ink_env::debug_println(&format!("calling get on {:?}", program_id));
 
-            let return_value: bool = build_call::<DefaultEnvironment>()
+            let return_value = build_call::<DefaultEnvironment>()
                 .callee(program_id) 
                 .gas_limit(20035585459) 
                 .transferred_value(0)
@@ -67,11 +63,11 @@ mod game {
                     ExecutionInput::new(Selector::new([0xDE, 0xAD, 0xBE, 0xFF]))
                 )
                 .returns::<ReturnType<bool>>()
-                .fire()
-                .unwrap();
+                .fire();
+                //.unwrap();
             
-            ink_env::debug_println(&format!("return value {}", return_value));
-            Ok(return_value)
+            ink_env::debug_println(&format!("return value {:?}", return_value));
+            Ok(true)
         }
     }
 
